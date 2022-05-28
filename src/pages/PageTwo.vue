fb9f14dd3f8a7482cdfa2513ee2a5d7010ea873a
projetos_b2ml <gabrieldaniel@gec.inatel.br>
2022-05-28 15:36:31 -0300

    Add swipper

--- src/pages/PageTwo.vue
+++ src/pages/PageTwo.vue
@@ -1,18 +1,45 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/Maps.svg"
-      style="width: 100vw; height: auto"
-    />
+  <q-page>
+    <swiper :modules="modules">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+    </swiper>
   </q-page>
 </template>
 
+<style scoped>
+.swiper {
+  max-width: 100vw;
+  height: 20vh;
+  padding: 0 20px 0 20px;
+  position: absolute;
+  bottom: 85px;
+}
+.swiper-pagination-bullet {
+  display: none !important;
+}
+</style>
+
 <script>
 import { defineComponent } from "vue";
+import SwiperClass, { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+
+// import swiper module styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default defineComponent({
   name: "PageTwo",
-  setup() {},
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Pagination],
+    };
+  },
 });
 </script>
