25b3f9c49ffaf15241fe465822e0906ad11fa8db
projetos_b2ml <gabrieldaniel@gec.inatel.br>
2022-05-29 13:08:09 -0300

    Update login screen

--- src/pages/LockScreen.vue
+++ src/pages/LockScreen.vue
@@ -1,96 +1,64 @@
 <template>
-  <q-layout class="bg-image" v-cloak>
-    <q-page-container>
-      <q-page class="flex flex-center">
-        <q-card
-          v-if="!$q.screen.lt.sm"
-          class="bg-transparent no-border no-shadow"
-        >
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar size="130px" class="shadow-10">
-                <img src="Profile.svg" />
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section class="text-white">
-              <q-item-label>Pratik Patel</q-item-label>
-              <q-item-label caption>
-                <q-input
-                  dark
-                  v-model="password"
-                  color="white"
-                  :type="isPwd ? 'password' : 'text'"
-                  placeholder="Enter Password"
-                >
-                  <template v-slot:append>
-                    <q-icon
-                      :name="isPwd ? 'visibility_off' : 'visibility'"
-                      class="cursor-pointer"
-                      @click="isPwd = !isPwd"
-                    />
-                  </template>
-                </q-input>
-              </q-item-label>
-              <q-item-label></q-item-label>
-            </q-item-section>
-            <q-item-section side center>
-              <q-btn
-                to="/"
-                round
-                flat
-                color="white"
-                class="q-mt-lg bg-blue-5"
-                icon="arrow_right_alt"
-              ></q-btn>
-            </q-item-section>
-          </q-item>
-        </q-card>
-        <q-card
-          v-if="$q.screen.lt.sm"
-          class="bg-transparent no-border no-shadow"
-        >
-          <q-card-section class="text-center">
-            <q-avatar size="130px" class="shadow-10" style="background: #fff">
-              <img src="~assets/Profile.svg" style="width: 45px" />
-            </q-avatar>
-          </q-card-section>
-          <q-card-section class="text-center">
-            <q-input
-              dark
-              v-model="user"
-              color="white"
-              type="text"
-              placeholder="Entre com o usuário"
-            />
-            <q-input
-              dark
-              v-model="password"
-              color="white"
-              :type="isPwd ? 'password' : 'text'"
-              placeholder="Entre com a senha"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPwd = !isPwd"
-                />
-              </template>
-            </q-input>
-            <q-btn
-              to="/"
-              round
-              flat
-              color="blue-3"
-              class="q-mt-lg bg-white"
-              icon="arrow_right_alt"
-            ></q-btn>
-          </q-card-section>
-        </q-card>
-      </q-page>
-    </q-page-container>
-  </q-layout>
+	<q-layout class="bg-image" v-cloak>
+		<q-page-container>
+			<q-page class="flex flex-center">
+				<q-card class="bg-transparent no-border no-shadow">
+					<q-card-section class="text-center">
+						<q-avatar
+							size="130px"
+							class="shadow-10"
+							style="
+								background: #fff;
+								margin-top: 30px;
+								margin-bottom: 10px;
+							"
+						>
+							<img
+								src="~assets/Profile.svg"
+								style="width: 45px"
+							/>
+						</q-avatar>
+					</q-card-section>
+					<q-card-section class="text-center">
+						<q-input
+							dark
+							v-model="email"
+							color="white"
+							type="email"
+							placeholder="Entre com o email"
+							:rules="emailRules"
+						/>
+						<q-input
+							dark
+							v-model="password"
+							color="white"
+							:type="isPwd ? 'password' : 'text'"
+							placeholder="Entre com a senha"
+							:rules="[(val) => !!val || 'Field is required']"
+						>
+							<template v-slot:append>
+								<q-icon
+									:name="
+										isPwd ? 'visibility_off' : 'visibility'
+									"
+									class="cursor-pointer"
+									@click="isPwd = !isPwd"
+								/>
+							</template>
+						</q-input>
+						<q-btn
+							to="/"
+							round
+							flat
+							color="blue-3"
+							class="q-mt-lg bg-white"
+							icon="arrow_right_alt"
+						></q-btn>
+					</q-card-section>
+				</q-card>
+			</q-page>
+		</q-page-container>
+	</q-layout>
 </template>
 
 <script>
@@ -98,25 +66,39 @@ import { defineComponent } from "vue";
 import { ref } from "vue";
 
 export default defineComponent({
-  name: "LockScreen",
+	name: "LockScreen",
 
-  setup() {
-    return {
-      user: ref(""),
-      password: ref(""),
-      isPwd: ref("password"),
-      isUsr: ref("user"),
-    };
-  },
+	setup() {
+		return {
+			email: ref(""),
+			password: ref(""),
+			isPwd: ref("password"),
+			isEmail: ref("email"),
+		};
+	},
+	watch: {
+		email: function (mail) {
+			if (mail !== "") {
+				this.emailRules = [
+					(v) =>
+						v.match(
+							/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+						) || "Invalid Email address",
+				];
+			} else if (mail === "") {
+				this.emailRules = [];
+			}
+		},
+	},
 });
 </script>
 
 <style>
 .bg-image {
-  background-image: linear-gradient(135deg, #42a5f5 0%, #d1f8fd 100%);
+	background-image: linear-gradient(135deg, #42a5f5 0%, #d1f8fd 100%);
 }
 
 [v-cloak] {
-  display: none !important;
+	display: none !important;
 }
 </style>
--- src/pages/RegisterScreen.vue
+++ src/pages/RegisterScreen.vue
@@ -1,83 +1,117 @@
 <template>
-  <q-layout class="bg-image" v-cloak>
-    <q-page-container>
-      <q-page class="flex flex-center" style="flex-direction: column">
-        <h1 class="title" style="color: #fff; font-size: 2rem">Cadastro</h1>
-        <q-card class="bg-transparent no-border no-shadow">
-          <q-item>
-            <q-item-section>
-              <q-input
-                dark
-                color="white"
-                dense
-                v-model="userRegister.name"
-                label="User Name"
-              />
+	<q-layout class="bg-image" v-cloak>
+		<q-page-container>
+			<q-page class="flex flex-center" style="flex-direction: column">
+				<h1 class="title" style="color: #fff; font-size: 2.3rem">
+					Cadastro
+				</h1>
+				<q-item-section avatar>
+					<q-avatar
+						size="130px"
+						class="shadow-10"
+						style="
+							background: #fff;
+							margin-top: 30px;
+							margin-bottom: 10px;
+						"
+					>
+						<img src="~assets/Profile.svg" style="width: 45px" />
+					</q-avatar>
+				</q-item-section>
+				<q-card class="bg-transparent no-border no-shadow">
+					<q-item>
+						<q-item-section>
+							<q-input
+								dark
+								color="white"
+								dense
+								v-model="user.name"
+								label="Nome"
+								:rules="[(val) => !!val || 'Field is required']"
+							/>
 
-              <q-input
-                dark
-                color="white"
-                dense
-                v-model="userRegister.email"
-                label="Email Address"
-              />
+							<p>{{ this.name }}</p>
 
-              <q-input
-                dark
-                v-model="password"
-                color="white"
-                :type="isPwd ? 'password' : 'text'"
-                placeholder="Enter Password"
-              >
-                <template v-slot:append>
-                  <q-icon
-                    :name="isPwd ? 'visibility_off' : 'visibility'"
-                    class="cursor-pointer"
-                    @click="isPwd = !isPwd"
-                  />
-                </template>
-              </q-input>
+							<q-input
+								dark
+								color="white"
+								dense
+								type="email"
+								v-model="user.email"
+								label="Email"
+								:rules="[
+									(v) =>
+										!v ||
+										/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+											v
+										) ||
+										'E-mail must be valid',
+								]"
+							/>
 
-              <q-input
-                dark
-                color="white"
-                autogrow
-                dense
-                v-model="userRegister.address"
-                label="Address"
-              />
-              <q-input
-                dark
-                color="white"
-                dense
-                type="number"
-                v-model="userRegister.phone"
-                label="Phone"
-              />
-            </q-item-section>
+							<q-input
+								dark
+								v-model="user.password"
+								color="white"
+								:type="isPwd ? 'password' : 'text'"
+								placeholder="Senha"
+								:rules="[(val) => !!val || 'Field is required']"
+							>
+								<template v-slot:append>
+									<q-icon
+										:name="
+											isPwd
+												? 'visibility_off'
+												: 'visibility'
+										"
+										class="cursor-pointer"
+										@click="isPwd = !isPwd"
+									/>
+								</template>
+							</q-input>
 
-            <q-item-section side center>
-              <q-btn
-                to="/"
-                round
-                flat
-                color="white"
-                class="q-mt-lg bg-blue-5"
-                icon="arrow_right_alt"
-                style="margin: 0 auto; margin-top: 24px"
-              ></q-btn>
-            </q-item-section>
-          </q-item>
-        </q-card>
-      </q-page>
-    </q-page-container>
-  </q-layout>
+							<q-input
+								dark
+								color="white"
+								autogrow
+								dense
+								v-model="user.address"
+								label="Endereço"
+								:rules="[(val) => !!val || 'Field is required']"
+							/>
+							<q-input
+								dark
+								color="white"
+								dense
+								type="number"
+								v-model="user.phone"
+								mask="(##) # ####-####"
+								label="Celular"
+								:rules="[(val) => !!val || 'Field is required']"
+							/>
+						</q-item-section>
+
+						<q-item-section side center>
+							<q-btn
+								to="/"
+								round
+								flat
+								color="blue-3"
+								class="q-mt-lg bg-white"
+								icon="arrow_right_alt"
+							></q-btn>
+						</q-item-section>
+					</q-item>
+				</q-card>
+			</q-page>
+		</q-page-container>
+	</q-layout>
 </template>
 
 <style scoped>
 .q-item {
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 }
 </style>
 
@@ -86,24 +120,38 @@ import { defineComponent } from "vue";
 import { ref } from "vue";
 
 export default defineComponent({
-  name: "RegisterScreen",
+	name: "RegisterScreen",
 
-  setup() {
-    return {
-      userRegister: {},
-      password: ref(""),
-      isPwd: ref("password"),
-    };
-  },
+	data() {
+		return {
+			user: { name: "", email: "", password: "", address: "", phone: "" },
+			model: "",
+			isPwd: ref("user.password"),
+		};
+	},
+	watch: {
+		email: function (mail) {
+			if (mail !== "") {
+				this.emailRules = [
+					(v) =>
+						v.match(
+							/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+						) || "Invalid Email address",
+				];
+			} else if (mail === "") {
+				this.emailRules = [];
+			}
+		},
+	},
 });
 </script>
 
 <style>
 .bg-image {
-  background-image: linear-gradient(135deg, #42a5f5 0%, #d1f8fd 100%);
+	background-image: linear-gradient(135deg, #42a5f5 0%, #d1f8fd 100%);
 }
 
 [v-cloak] {
-  display: none !important;
+	display: none !important;
 }
 </style>
